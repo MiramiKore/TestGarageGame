d1483157ce8176c4ce3414ddcd9e872c5f7b778e
Mirami <153764172+MiramiKore@users.noreply.github.com>
2024-10-22 18:06:03 +0300

    Сделал прототип системы подбора предметов

---
+++ Assets/Scripts/ItemSelectionSystem/ItemSelection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace itemSelectionSystem
+{
+    public class ItemSelection : MonoBehaviour
+    {
+        // Список доступных предметов, находящихся рядом с игроком
+        private readonly List<GameObject> _itemsInRange = new List<GameObject>();
+        
+        private ItemSelectionSystem _itemSelectionSystem;
+        
+        private void Awake()
+        {
+            _itemSelectionSystem = FindObjectOfType<ItemSelectionSystem>();
+        }
+
+        // Подходя к предмету, добавляем его в список доступных для подбора предметов
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Item"))
+            {
+                _itemsInRange.Add(other.gameObject);
+            }
+        }
+        
+        // Отходя от предмета, убираем его из списка доступных
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Item"))
+            {
+                _itemsInRange.Remove(other.gameObject);
+            }
+        }
+        
+        private void Update()
+        {
+            // Если в зоне есть предметы и игрок нажимает клавишу клавишу подбора предмета
+            if (_itemsInRange.Count > 0 && Input.GetKeyDown(KeyCode.E))
+            {
+                // Подбираем первый предмет из списка
+                _itemSelectionSystem.GetItem(_itemsInRange[0]);
+                
+                _itemsInRange[0].SetActive(false);
+
+                // Удаляем предмет из списка после подбора
+                _itemsInRange.RemoveAt(0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ItemSelectionSystem/ItemSelectionSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace itemSelectionSystem
+{
+    public class ItemSelectionSystem : MonoBehaviour
+    {
+        // Список подобранных предметов
+        public List<GameObject> items = new List<GameObject>();
+        
+        public void GetItem(GameObject currentObject)
+        {
+            items.Add(currentObject);
+        }
+
+        public void GiveItem()
+        {
+            
+        }
+    }
+}
